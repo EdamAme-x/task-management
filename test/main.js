f4267657ae6852e7a30621e12ee43456cf9c0473
256-daisuki <256mitdaisuki+git@gmail.com>
2023-12-25 16:07:01 +0900

    ファイル移動とかした気がする

---
+++ test/main.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+// ご主人さまのMariaDBの情報に合わせて変更してね
+const connection = mysql.createConnection({
+  host: '192.168.100.15',
+  user: 'robot',
+  password: '',
+  database: 'test',
+  port: 3306
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('エラーが発生しましたにゃん！', err);
+  } else {
+    console.log('MariaDBに接続できましたにゃん！');
+  }
+});
+
+// ちゃんと接続を切るにゃん！
+connection.end();
